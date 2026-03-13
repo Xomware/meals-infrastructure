5da8298d93c2d19725f7788303edeff74f5acd4e
domgiordano <dominickj.giordano@gmail.com>
2026-03-13 17:05:50 -0400

    cost: upgrade WAF module to v2.0.0 (4→2 rules)

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -1,5 +1,5 @@
 module "waf_cloudfront" {
-  source = "git::https://github.com/domgiordano/waf.git?ref=v1.1.0"
+  source = "git::https://github.com/domgiordano/waf.git?ref=v2.0.0"
 
   app_name = "${var.app_name}-cloudfront"
   scope    = "CLOUDFRONT"
